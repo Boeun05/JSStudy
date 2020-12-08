fbbfff0acbcce4576f1d0a4fcc62244b7981d334
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2020-12-08 23:04:30 +0900

    좌표 성능개선(top, left => translate)

--- browser/chap4/coordinates_007/coordinates/main.js
+++ browser/chap4/coordinates_007/coordinates/main.js
@@ -2,16 +2,21 @@ const tag = document.querySelector('.tag')
 const horizontal = document.querySelector('.horizontal')
 const vertical = document.querySelector('.vertical')
 const image = document.querySelector('.image')
-document.addEventListener('mousemove', (e) => {
-    const x = e.clientX
-    const y = e.clientY
-    tag.textContent = `${x},${y}`
-    tag.style.left = `${x}px`
-    tag.style.top = `${y}px`
-    vertical.style.left = `${x}px`
-    horizontal.style.top = `${y}px`
-    image.style.left = `${x}px`
-    image.style.top = `${y}px`
+
+window.addEventListener('load', () => {
+    const imageRect = image.getBoundingClientRect()
+    const imageWidth = imageRect.width / 2
+    const imageHeight = imageRect.height / 2
+
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX
+        const y = e.clientY
+        tag.textContent = `${x},${y}`
+        tag.style.transform = `translate(${x + 20}px, ${y + 20}px)`
+        vertical.style.transform = `translateX(${x}px)`
+        horizontal.style.transform = `translateY(${y}px)`
+        image.style.transform = `translate(${x - imageWidth}px , ${y - imageHeight}px)`
+    })
 })
 
 
